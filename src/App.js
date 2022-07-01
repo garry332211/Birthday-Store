9fc572165ee5555560657805fb2c8b447d7aee03
garry332211 <68804397+garry332211@users.noreply.github.com>
2022-07-01 17:13:27 +1200

    New Birthday App Base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 
-import Header from "./components/Header";
 
 
 function App() {
   return (
     <React.Fragment>
-    <Header />
+   
     </React.Fragment>
   );
 }
